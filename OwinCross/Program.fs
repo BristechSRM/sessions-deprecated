baff47c49be2c7b90321e898e8b50d23d4e40098
Lauren Clark <lclark@scottlogic.com>
2016-03-04 16:26:56 +0000

    Add speaker statuses

--- OwinCross/Program.fs
+++ OwinCross/Program.fs
@@ -61,6 +61,14 @@ type ValuesController() =
         printfn "Received Get for Value %d" id
         sprintf "value %d" id
 
+type SpeakerStatus =
+    | Unassigned = 0
+    | Assigned = 1
+    | InProgress = 2
+    | Deferred = 3
+    | TopicApproved = 4
+    | DateAssigned = 5
+
 type Speaker = {
     Name : string
     Title : string
@@ -68,6 +76,7 @@ type Speaker = {
     Admin: string
     AdminImageUrl: string
     LastContacted: string
+    SpeakerStatus: SpeakerStatus
 }
 
 type SpeakersController() =
@@ -76,9 +85,11 @@ type SpeakersController() =
     member x.Get() =
         printfn "Received Get Request for Speakers"
         seq {
-            yield {Name = "Thomas Hull"; Title = "To know javascript is to love javascript"; Rating=5; Admin="David Wybourn"; AdminImageUrl="https://placebear.com/50/50"; LastContacted="1970-01-01"; }
-            yield {Name = "Jason Ebbin";   Title = "How dull is F#"; Rating=3; Admin="Jason Ebbin"; AdminImageUrl="https://placebear.com/50/50"; LastContacted="1989-11-09" }
-            yield {Name = "David Wybourn"; Title = "Concourse: Where I met myself"; Rating=5; Admin="Chris James Smith"; AdminImageUrl="https://placebear.com/50/50"; LastContacted="2015-10-21" }
+            yield {Name = "Thomas Hull"; Title = "To know javascript is to love javascript"; Rating=5; Admin="David Wybourn"; AdminImageUrl="https://placebear.com/50/50"; LastContacted="1970-01-01"; SpeakerStatus = SpeakerStatus.Assigned }
+            yield {Name = "Jason Ebbin";   Title = "How dull is F#"; Rating=3; Admin="Jason Ebbin"; AdminImageUrl="https://placebear.com/50/50"; LastContacted="1989-11-09"; SpeakerStatus = SpeakerStatus.Deferred }
+            yield {Name = "David Wybourn"; Title = "Concourse: Where I met myself"; Rating=5; Admin="Chris James Smith"; AdminImageUrl="https://placebear.com/50/50"; LastContacted="2015-10-21"; SpeakerStatus = SpeakerStatus.TopicApproved }
+            yield {Name = "Joe Bloggs"; Title = ""; Rating=0; Admin="Thomas Hull"; AdminImageUrl="https://placebear.com/50/50"; LastContacted="2016-02-19"; SpeakerStatus = SpeakerStatus.InProgress }
+            yield {Name = "Chris Smith"; Title = "C# or F#: Which is sharper?"; Rating=4; Admin="Thomas Hull"; AdminImageUrl="https://placebear.com/50/50"; LastContacted="2016-01-10"; SpeakerStatus = SpeakerStatus.DateAssigned }
         }
 
 [<EntryPoint>]
