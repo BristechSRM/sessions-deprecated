b391134956f928682d725f9c876eb20e9e07b406
Lauren Clark <lclark@scottlogic.com>
2016-03-28 13:41:30 +0100

    Revert "Merge pull request #11 from BristechSRM/98-TrelloDataViaJson"

--- Speakers/Repositories.fs
+++ Speakers/Repositories.fs
@@ -3,27 +3,14 @@
 open System
 open Speakers.Models
 open Speakers.Entities
-
-
-(*
-Excluded and replaced with json import until sql connection issues are fixed
 open System.Configuration
 open MySql.Data.MySqlClient
 open Dapper
-
 
 let connectionString = ConfigurationManager.ConnectionStrings.Item("DefaultConnection").ConnectionString
 
 let connection = new MySqlConnection(connectionString)
 connection.Open()
-
-*)
-
-open Newtonsoft.Json
-open System.IO
-
-let importFilePath = @"outlines-import.json"
-let talkOutlines = JsonConvert.DeserializeObject<TalkOutlineEntity seq>(File.ReadAllText(importFilePath))
 
 let entityToTalkOutline (entity: TalkOutlineEntity) =
     {
@@ -39,12 +26,8 @@ let entityToTalkOutline (entity: TalkOutlineEntity) =
     }
 
 let getAllTalkOutlines =
-    talkOutlines 
+    connection.Query<TalkOutlineEntity>("select * from talk_outlines")
     |> Seq.map entityToTalkOutline
-//    Excluded and replaced with json import until sql connection issues are fixed
-//    connection.Query<TalkOutlineEntity>("select * from talk_outlines")
-//    |> Seq.map entityToTalkOutline
-
 
 
 
