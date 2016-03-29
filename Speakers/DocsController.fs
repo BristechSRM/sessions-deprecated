3ead25def20299abc0d18c14669320c944f9b66a
ilovell <ilovell@scottlogic.co.uk>
2016-03-29 13:19:10 +0100

    Added docs controller to expose Swagger API docs

---
+++ Speakers/DocsController.fs
@@ -0,0 +1,15 @@
+﻿module DocsController
+
+open Newtonsoft.Json.Linq
+open System.IO
+open System.Net
+open System.Net.Http
+open System.Web.Http
+open Speakers.Repositories
+
+type DocsController() =
+    inherit ApiController()
+
+    member x.Get() =                
+        let docs = JObject.Parse(File.ReadAllText("api.json"));
+        x.Request.CreateResponse(HttpStatusCode.OK, docs, "application/json")
