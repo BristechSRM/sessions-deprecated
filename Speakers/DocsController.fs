2f8b0968e579c38736e6d84047bf4c8bc86fc7dc
ilovell <ilovell@scottlogic.co.uk>
2016-03-31 15:27:52 +0100

    Renamed controllers and removed this reference

--- Speakers/DocsController.fs
+++ Speakers/DocsController.fs
@@ -8,12 +8,12 @@ open System.Web.Http
 open Speakers.Repositories
 open FSharp.Data
 
-type DocsController() as this =
+type DocsController() =
     inherit ApiController()
 
     member x.Get() =
-        use stream = this.GetType().Assembly.GetManifestResourceStream("api.json")
+        use stream = x.GetType().Assembly.GetManifestResourceStream("api.json")
         use streamReader = new StreamReader(stream)
         let content = streamReader.ReadToEnd()
         let docs = JObject.Parse(content);        
-        x.Request.CreateResponse(HttpStatusCode.OK, docs, "application/json");
+        x.Request.CreateResponse(HttpStatusCode.OK, docs, "application/json")
