c032a3ec3467747cd6bd41a55f1388e823ebac93
Chris Smith <csmith@scottlogic.co.uk>
2016-04-15 14:24:17 +0100

    Move serialization configuration into separate assembly

---
+++ Bristech.Srm.HttpConfig/Serialization.fs
@@ -0,0 +1,26 @@
+﻿namespace Bristech.Srm.HttpConfig
+
+module Serialization =
+
+    open System.Web.Http
+    open Newtonsoft.Json
+    open System.Net.Http.Headers
+    open Serilog
+
+    let configure (config: HttpConfiguration) =
+        config.Formatters.XmlFormatter.UseXmlSerializer <- true
+
+        // The following lines are for controlling the serialisation of F# properties from Records and Unions
+        config.Formatters.JsonFormatter.SerializerSettings.ContractResolver <- Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver ()
+
+        config.Formatters.JsonFormatter.SerializerSettings.MissingMemberHandling <- MissingMemberHandling.Error
+        config.Formatters.JsonFormatter.SerializerSettings.Error <- new System.EventHandler<Serialization.ErrorEventArgs>(fun _ errorEvent ->
+            let context = System.Web.HttpContext.Current
+            let error = errorEvent.ErrorContext.Error
+            context.AddError(error)
+            errorEvent.ErrorContext.Handled <- true)
+        config.Formatters.JsonFormatter.SupportedMediaTypes.Add(MediaTypeHeaderValue("text/html"))
+
+        Log.Information("Configured JSON serialisation")
+
+        config
--- Sessions/Startup.fs
+++ Sessions/Startup.fs
@@ -8,30 +8,12 @@ open Serilog
 
 module private ConfigurationHelpers =
 
-    let configureSerializationFormatters (config : HttpConfiguration) =
-        config.Formatters.XmlFormatter.UseXmlSerializer <- true
-
-        //The following lines are for controlling the serialisation of F# properties from Records and Unions
-        config.Formatters.JsonFormatter.SerializerSettings.ContractResolver <- Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver ()
-
-        config.Formatters.JsonFormatter.SerializerSettings.MissingMemberHandling <- MissingMemberHandling.Error
-        config.Formatters.JsonFormatter.SerializerSettings.Error <- new System.EventHandler<Serialization.ErrorEventArgs>(fun _ errorEvent ->
-            let context = System.Web.HttpContext.Current
-            let error = errorEvent.ErrorContext.Error
-            context.AddError(error)
-            errorEvent.ErrorContext.Handled <- true)
-        config.Formatters.JsonFormatter.SupportedMediaTypes.Add(MediaTypeHeaderValue("text/html"))
-
-        Log.Information("Configured JSON serialisation")
-
-        config
-
     let registerConfiguration (config : HttpConfiguration) =
         config
         |> Bristech.Srm.HttpConfig.Logging.configure
         |> Bristech.Srm.HttpConfig.Cors.configure
         |> Bristech.Srm.HttpConfig.Routes.configure
-        |> configureSerializationFormatters
+        |> Bristech.Srm.HttpConfig.Serialization.configure
 
 type Startup() =
     member __.Configuration (appBuilder: IAppBuilder) =
