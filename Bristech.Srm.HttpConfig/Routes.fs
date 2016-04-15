21f0d6600e59a846d42ed865a9ba70c5966def30
Chris Smith <csmith@scottlogic.co.uk>
2016-04-15 14:19:51 +0100

    Move routes configuration into separate assembly

---
+++ Bristech.Srm.HttpConfig/Routes.fs
@@ -0,0 +1,13 @@
+﻿namespace Bristech.Srm.HttpConfig
+
+module Routes =
+    
+    open System.Web.Http
+    open Serilog
+
+    let configure (config : HttpConfiguration) =
+        let routes = config.Routes
+        let route = routes.MapHttpRoute("DefaultApi", "{controller}/{id}")
+        route.Defaults.Add("id", RouteParameter.Optional)
+        Log.Information("Configured API routing")
+        config
--- Sessions/Startup.fs
+++ Sessions/Startup.fs
@@ -8,13 +8,6 @@ open Serilog
 
 module private ConfigurationHelpers =
 
-    let configureRoutes (config : HttpConfiguration) =
-        let routes = config.Routes
-        let route = routes.MapHttpRoute("DefaultApi", "{controller}/{id}")
-        route.Defaults.Add("id", RouteParameter.Optional)
-        Log.Information("Configured API routing")
-        config
-
     let configureSerializationFormatters (config : HttpConfiguration) =
         config.Formatters.XmlFormatter.UseXmlSerializer <- true
 
@@ -37,7 +30,7 @@ module private ConfigurationHelpers =
         config
         |> Bristech.Srm.HttpConfig.Logging.configure
         |> Bristech.Srm.HttpConfig.Cors.configure
-        |> configureRoutes
+        |> Bristech.Srm.HttpConfig.Routes.configure
         |> configureSerializationFormatters
 
 type Startup() =
