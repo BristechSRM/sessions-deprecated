3f9d21d544ec08d3c8fa4fa01625e163e34374ff
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-06-16 15:48:32 +0100

    Extra logging

--- Sessions/ProfilesController.fs
+++ Sessions/ProfilesController.fs
@@ -12,20 +12,32 @@ type ProfilesController() =
     inherit ApiController()
     
     member x.Get(id : Guid) = 
-        Log.Information("Received GET request for profile with id {id}", id)
+        Log.Information("Received GET request for profile with id: {id}", id)
         match getProfile id with
-        | Success profile -> x.Request.CreateResponse(profile)
-        | Failure error -> x.Request.CreateErrorResponse(error.HttpStatus, error.Message)
+        | Success profile -> 
+            Log.Information("Success: Post request for profile with id: {id} complete", id)
+            x.Request.CreateResponse(profile)
+        | Failure error -> 
+            Log.Information("Failure: Put request for profile with id: {id} failed with status code: {code} and message: {message}", id, error.HttpStatus, error.Message)
+            x.Request.CreateErrorResponse(error.HttpStatus, error.Message)
     
     //TODO validation
     member x.Post(newProfile : Profile) = 
         Log.Information("Received Post request for new profile")
         match addProfile newProfile with
-        | Success id -> x.Request.CreateResponse(HttpStatusCode.Created, id)
-        | Failure error -> x.Request.CreateErrorResponse(error.HttpStatus, error.Message)
+        | Success id -> 
+            Log.Information("Success: Post request for profile with id: {id} complete", id)
+            x.Request.CreateResponse(HttpStatusCode.Created, id)
+        | Failure error -> 
+            Log.Information("Failure: Post request for profile with id: {id} failed with status code: {code} and message: {message}", id, error.HttpStatus, error.Message)
+            x.Request.CreateErrorResponse(error.HttpStatus, error.Message)
 
     member x.Put(id: Guid, updatedProfile : Profile) = 
-        Log.Information("Received Put request for profile with id {id}", id)
+        Log.Information("Received Put request for profile with id: {id}", id)
         match updateProfile id updatedProfile with
-        | Success _ -> x.Request.CreateResponse(HttpStatusCode.OK)
-        | Failure error -> x.Request.CreateErrorResponse(error.HttpStatus, error.Message)
+        | Success _ -> 
+            Log.Information("Success: Put request for profile with id: {id} complete", id)
+            x.Request.CreateResponse(HttpStatusCode.OK)
+        | Failure error -> 
+            Log.Information("Failure: Put request for profile with id: {id} failed with status code: {code} and message: {message}", id, error.HttpStatus, error.Message)
+            x.Request.CreateErrorResponse(error.HttpStatus, error.Message)
