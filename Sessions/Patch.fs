0d7cd8a471da4ca9fd7fc96e7a2fe5d402a3d6d2
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-06-21 08:26:57 +0100

    Add Patch module

---
+++ Sessions/Patch.fs
@@ -0,0 +1,59 @@
+﻿module Patch
+
+open System
+open System.Net
+open Models
+
+let isNullOrWhiteSpaceOption str = 
+    if String.IsNullOrWhiteSpace str then 
+        None 
+    else 
+        Some str
+
+let parseOp op = 
+    op
+    |> isNullOrWhiteSpaceOption
+    |> Option.map (fun str -> str.ToLower())
+
+let parsePath path = 
+    let p = 
+        path
+        |> isNullOrWhiteSpaceOption
+        |> Option.filter (fun str -> str.StartsWith "/")
+        |> Option.map (fun str -> (str.ToLower().Split [|'/'|]) |> Array.skip 1 )
+    match p with
+    | Some path -> Success path
+    | None -> Failure <| sprintf "'path' field: %s could not be correctly parsed" path
+
+let validValue value = 
+    if String.IsNullOrWhiteSpace value then 
+        Failure <| sprintf "'value' field: %s was null, whitespace or missing" value
+    else 
+        Success value
+
+let createFailMessage fieldMessage (rawOp: RawPatchOperation) = 
+    sprintf "%s. Operation was: %A" fieldMessage rawOp
+
+let parseOperation (rawOp: RawPatchOperation) =
+    match parseOp rawOp.Op with 
+    | Some "replace" -> 
+        match parsePath rawOp.Path with
+        | Success path -> 
+            match validValue rawOp.Value with
+            | Success value -> Success <| Replace(path, value)
+            | Failure message -> Failure message
+        | Failure message -> Failure message
+    | _ -> Failure <| sprintf "'op' field: %s did not match an accepted patch operation" rawOp.Op
+
+let parseOperations (rawOps: RawPatchOperation list option) = 
+    match rawOps with 
+    | Some ops -> 
+        let parsedOps = ops |> List.map (parseOperation) 
+        let failedOps = parsedOps |> List.choose(function | Failure message -> Some message | _ -> None)
+        match failedOps with
+        | [] ->  parsedOps |> List.choose(function | Success op -> Some op | _ -> None) |> Success
+        | failedOps -> 
+            let messages = "Parsing of input operations list failed with the following errors:"::failedOps
+            Failure { HttpStatus = HttpStatusCode.BadRequest; Message = messages |> String.concat "\n"}
+    | None -> Failure { HttpStatus = HttpStatusCode.BadRequest; Message = "Input operations list was null "}
+
--- Sessions/ProfilesController.fs
+++ Sessions/ProfilesController.fs
@@ -41,3 +41,18 @@ type ProfilesController() =
         | Failure error -> 
             Log.Error("Put request for profile with id: {id} failed with status code: {code} and message: {message}", id, error.HttpStatus, error.Message)
             x.Request.CreateErrorResponse(error.HttpStatus, error.Message)
+
+    member x.Patch(id: Guid, operations : RawPatchOperation list option) = 
+        Log.Information("Received Patch request for profile with id: {id}")
+        match Patch.parseOperations operations with 
+        | Success ops -> 
+            match patchProfile id ops with
+            | Success () -> 
+                Log.Information("Success: Patch request for profile with id: {id} complete", id)
+                x.Request.CreateResponse(HttpStatusCode.OK)
+            | Failure error -> 
+                Log.Error("Patch request profile with id {id} failed with status code: {code} and message: {message",id,error.HttpStatus,error.Message)
+                x.Request.CreateErrorResponse(error.HttpStatus,error.Message)
+        | Failure error -> 
+            Log.Error("Could not parse patch operations list. Failed with status code: {code} and message {message}", error.HttpStatus, error.Message)
+            x.Request.CreateErrorResponse(error.HttpStatus, error.Message)
